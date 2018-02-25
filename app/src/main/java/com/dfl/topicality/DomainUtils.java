6bcd5f7dd9664a19219ee5151834daa853d30762
Diogo Loureiro <diogofialholoureiro@gmail.com>
2018-02-25 11:02:34 +0000

    removed exception from domain utils; updated strings;

--- app/src/main/java/com/dfl/topicality/DomainUtils.java
+++ app/src/main/java/com/dfl/topicality/DomainUtils.java
@@ -11,12 +11,11 @@ public class DomainUtils {
 
     public static String getDomainName(String url) {
         try {
-            URI uri = new URI(url);
-            String domain = uri.getHost();
+            String domain = new URI(url.replaceAll(" ","")).getHost();
             return domain.startsWith("www.") ? domain.substring(4) : domain;
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
-        throw new IllegalArgumentException(url);
+        return null;
     }
 }
